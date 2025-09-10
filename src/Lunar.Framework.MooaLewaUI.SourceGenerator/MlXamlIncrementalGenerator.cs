6fde81495d45c565c55cd01f4ff54dfe3c4633e1
Bli_Aik <haikun2333@126.com>
2025-09-10 13:43:09 +0800

    chore(core): downgrade Microsoft.CodeAnalysis.CSharp to 3.8.0
    Refactor source generator from incremental to standard ISourceGenerator implementation. Remove unused class.mlxaml file.

--- src/Lunar.Framework.MooaLewaUI.SourceGenerator/MlXamlIncrementalGenerator.cs
+++ src/Lunar.Framework.MooaLewaUI.SourceGenerator/MlXamlIncrementalGenerator.cs
@@ -1,44 +1,46 @@
 using System;
-using System.IO;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 
 namespace Lunar.Framework.MooaLewaUI.SourceGenerator;
 
 [Generator]
-public class MlXamlIncrementalGenerator : IIncrementalGenerator
+public class MlXamlSourceGenerator : ISourceGenerator
 {
-    public void Initialize(IncrementalGeneratorInitializationContext context)
+    public void Initialize(GeneratorInitializationContext context)
     {
-        var mlxamlFiles = context.AdditionalTextsProvider
-            .Where(static file => file.Path.EndsWith(".mlxaml", StringComparison.OrdinalIgnoreCase));
+        // ISourceGenerator doesn't use this method for our purposes.
+        // In more complex scenarios, could register a syntax receiver here.
+    }
 
-        var parsedProvider = mlxamlFiles.Select((file, cancellationToken) =>
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var mlxamlFiles = context.AdditionalFiles
+            .Where(file => file.Path.EndsWith(".mlxaml", StringComparison.OrdinalIgnoreCase));
+
+        foreach (var file in mlxamlFiles)
         {
-            var xmlContent = file.GetText(cancellationToken)?.ToString() ?? "";
+            var xmlContent = file.GetText(context.CancellationToken)?.ToString() ?? "";
+
             var astNodes = MlXamlParser.Parse(xmlContent, out var diagnostics);
+
+            foreach (var diagnostic in diagnostics)
+            {
+                context.ReportDiagnostic(diagnostic);
+            }
+
+            if (diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error))
+            {
+                continue;
+            }
+
             var className = GeneratorHelper.SanitizeClassName(file.Path);
             var hintName = $"{className}.g.cs";
 
-            return (HintName: hintName, AstNodes: astNodes, Diagnostics: diagnostics, ClassName: className);
-        });
-
-        context.RegisterSourceOutput(parsedProvider, (sourceProductionContext, parsedData) =>
-        {
-            foreach (var diagnostic in parsedData.Diagnostics)
-            {
-                sourceProductionContext.ReportDiagnostic(diagnostic);
-            }
-
-            if (parsedData.Diagnostics.Any(d => d.Severity == DiagnosticSeverity.Error))
-            {
-                return;
-            }
-
-            var syntaxTree = MlXamlCodeGenerator.Generate(parsedData.AstNodes, parsedData.ClassName);
+            var syntaxTree = MlXamlCodeGenerator.Generate(astNodes, className);
             var sourceCode = syntaxTree.GetRoot().ToFullString();
 
-            sourceProductionContext.AddSource(parsedData.HintName, sourceCode);
-        });
+            context.AddSource(hintName, sourceCode);
+        }
     }
 }
